1775f97e775137eca4f766b163a9a84015ee7150
amouhzi <github@anezi.net>
2013-11-03 13:49:21 +0100

    Use TES_URL const

--- tests/CurlTest.php
+++ tests/CurlTest.php
@@ -3,24 +3,24 @@
 namespace Curl;
 
 class CurlTest extends \PHPUnit_Framework_TestCase {
-	
+
 	const TEST_URL = 'http://php-curl-test.anezi.net/tests/server.php';
-	
+
 	/**
-	 * 
+	 *
 	 * @var Curl
 	 */
 	protected $curl;
-	
+
 	function setUp() {
 		$this->curl = new Curl();
 		$this->curl->setOpt(CURLOPT_SSL_VERIFYPEER, FALSE);
 		$this->curl->setOpt(CURLOPT_SSL_VERIFYHOST, FALSE);
 	}
-	
-	function server($url, $request_method, $data='') {
+
+	function server($request_method, $data='') {
 		$request_method = strtolower($request_method);
-		$this->curl->$request_method($url, $data);
+		$this->curl->$request_method(self::TEST_URL, $data);
 		return $this->curl->response;
 	}
 
@@ -32,7 +32,7 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 	public function testUserAgent() {
 			
 		$this->curl->setUserAgent(Curl::USER_AGENT);
-		$this->assertEquals(Curl::USER_AGENT, $this->server(self::TEST_URL, 'GET', array(
+		$this->assertEquals(Curl::USER_AGENT, $this->server('GET', array(
 				'test' => 'server',
 				'key' => 'HTTP_USER_AGENT',
 		)));
@@ -40,28 +40,28 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGet() {
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'server',
 				'key' => 'REQUEST_METHOD',
 		)) === 'GET');
 	}
 
 	public function testPostRequestMethod() {
-		$this->assertTrue($this->server(self::TEST_URL, 'POST', array(
+		$this->assertTrue($this->server('POST', array(
 				'test' => 'server',
 				'key' => 'REQUEST_METHOD',
 		)) === 'POST');
 	}
 
 	public function testPostData() {
-		$this->assertTrue($this->server(self::TEST_URL, 'POST', array(
+		$this->assertTrue($this->server('POST', array(
 				'test' => 'post',
 				'key' => 'test',
 		)) === 'post');
 	}
 
 	public function testPostMultidimensionalData() {
-		$this->assertTrue($this->server(self::TEST_URL, 'POST', array(
+		$this->assertTrue($this->server('POST', array(
 				'test' => 'post_multidimensional',
 				'key' => 'file',
 				'file' => array(
@@ -75,7 +75,7 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 	public function testPostFilePathUpload() {
 		$file_path = $this->get_png();
 
-		$this->assertTrue($this->server(self::TEST_URL, 'POST', array(
+		$this->assertTrue($this->server('POST', array(
 				'test' => 'post_file_path_upload',
 				'key' => 'image',
 				'image' => '@' . $file_path,
@@ -85,14 +85,14 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testPutRequestMethod() {
-		$this->assertTrue($this->server(self::TEST_URL, 'PUT', array(
+		$this->assertTrue($this->server('PUT', array(
 				'test' => 'server',
 				'key' => 'REQUEST_METHOD',
 		)) === 'PUT');
 	}
 
 	public function testPutData() {
-		$this->assertTrue($this->server(self::TEST_URL, 'PUT', array(
+		$this->assertTrue($this->server('PUT', array(
 				'test' => 'put',
 				'key' => 'test',
 		)) === 'put');
@@ -115,26 +115,26 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testDelete() {
-		$this->assertTrue($this->server(self::TEST_URL, 'DELETE', array(
+		$this->assertTrue($this->server('DELETE', array(
 				'test' => 'server',
 				'key' => 'REQUEST_METHOD',
 		)) === 'DELETE');
 
-		$this->assertTrue($this->server(self::TEST_URL, 'DELETE', array(
+		$this->assertTrue($this->server('DELETE', array(
 				'test' => 'delete',
 				'key' => 'test',
 		)) === 'delete');
 	}
 
 	public function testBasicHttpAuth() {
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'http_basic_auth',
 		)) === 'canceled');
 
 		$username = 'myusername';
 		$password = 'mypassword';
 		$this->curl->setBasicAuthentication($username, $password);
-		$this->server(self::TEST_URL, 'GET', array(
+		$this->server('GET', array(
 				'test' => 'http_basic_auth',
 		));
 		$json = json_decode($this->curl->response);
@@ -144,7 +144,7 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 
 	public function testReferrer() {
 		$this->curl->setReferrer('myreferrer');
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'server',
 				'key' => 'HTTP_REFERER',
 		)) === 'myreferrer');
@@ -152,7 +152,7 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 
 	public function testCookies() {
 		$this->curl->setCookie('mycookie', 'yum');
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'cookie',
 				'key' => 'mycookie',
 		)) === 'yum');
@@ -170,20 +170,20 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 		$this->curl->setHeader('Content-Type', 'application/json');
 		$this->curl->setHeader('X-Requested-With', 'XMLHttpRequest');
 		$this->curl->setHeader('Accept', 'application/json');
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'server',
 				'key' => 'CONTENT_TYPE',
 		)) === 'application/json');
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'server',
 				'key' => 'HTTP_X_REQUESTED_WITH',
 		)) === 'XMLHttpRequest');
-		$this->assertTrue($this->server(self::TEST_URL, 'GET', array(
+		$this->assertTrue($this->server('GET', array(
 				'test' => 'server',
 				'key' => 'HTTP_ACCEPT',
 		)) === 'application/json');
 	}
-	
+
 	function create_png() {
 		// PNG image data, 1 x 1, 1-bit colormap, non-interlaced
 		ob_start();
@@ -192,14 +192,14 @@ class CurlTest extends \PHPUnit_Framework_TestCase {
 		ob_end_clean();
 		return $raw_image;
 	}
-	
+
 	function create_tmp_file($data) {
 		$tmp_file = tmpfile();
 		fwrite($tmp_file, $data);
 		rewind($tmp_file);
 		return $tmp_file;
 	}
-	
+
 	function get_png() {
 		$tmp_filename = tempnam('/tmp', 'php-curl-class.');
 		file_put_contents($tmp_filename, $this->create_png());
